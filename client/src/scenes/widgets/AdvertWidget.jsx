d19f16e846905460fe7f74868bcbe949651916c1
Prabakaran.M <karanpk0108@gmail.com>
2023-09-18 16:59:18 +0530

    modify

--- client/src/scenes/widgets/AdvertWidget.jsx
+++ client/src/scenes/widgets/AdvertWidget.jsx
@@ -20,16 +20,17 @@ const AdvertWidget = () => {
         width="100%"
         height="auto"
         alt="advert"
-        src="http://localhost:3001/assets/info4.jpeg"
+        src="http://localhost:3001/assets/Internet-Vector-Illustration.jpg"
         style={{ borderRadius: "0.75rem", margin: "0.75rem 0" }}
       />
       <FlexBetween>
-        <Typography color={main}>MikaCosmetics</Typography>
-        <Typography color={medium}>mikacosmetics.com</Typography>
+        <Typography color={main}>ADD Title</Typography>
+        <Typography color={medium}>ADD Link</Typography>
       </FlexBetween>
       <Typography color={medium} m="0.5rem 0">
-        Your pathway to stunning and immaculate beauty and made sure your skin
-        is exfoliating skin and shining like light.
+        Mauris ac congue neque, vitae viverra orci. Nunc consectetur dolor a
+        diam volutpat, nec scelerisque nulla vulputate. Donec vitae faucibus
+        tortor, vitae hendrerit odio.
       </Typography>
     </WidgetWrapper>
   );
