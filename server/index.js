cefafd2233f6b5e3a8085591d0d3ce94977117e8
Prabakaran.M <karanpk0108@gmail.com>
2024-01-05 01:22:57 +0530

    Added frontend orign link

--- server/index.js
+++ server/index.js
@@ -31,7 +31,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(
   cors({
-    origin: ["https://social-eyes.vercel.app/"],
+    origin: ["https://social-eyes-35kf.vercel.app"],
     methods: ["POST", "GET"],
     credentials: true,
   })
