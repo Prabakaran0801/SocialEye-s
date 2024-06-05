ef269f22d5fa655733b98249ee39af20a4a3d99d
Prabakaran <karanpk0108@gmail.com>
2024-06-05 19:02:48 +0530

    fix

--- client/src/scenes/loginPage/Form.jsx
+++ client/src/scenes/loginPage/Form.jsx
@@ -79,7 +79,7 @@ const Form = () => {
   };
 
   const login = async (values, onSubmitProps) => {
-    const loggedInResponse = await fetch(`http://localhost:3000/auth/login`, {
+    const loggedInResponse = await fetch(`https://api.render.com/deploy/srv-cpg622f79t8c73eel3l0?key=cU-phVJLOhk/auth/login`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(values),
