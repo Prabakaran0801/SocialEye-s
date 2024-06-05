f00ece906e6e808972467f69b7696b7f10fde507
Prabakaran <karanpk0108@gmail.com>
2024-06-05 19:02:48 +0530

    fix

--- server/index.js
+++ server/index.js
@@ -51,10 +51,9 @@ app.use((req, res, next) => {
 });
 // Set CSP header middleware
 app.use((req, res, next) => {
-  res.setHeader(
-    "Content-Security-Policy",
-    "default-src 'self'; connect-src http://localhost:3000;"
-  );
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
 });
 app.use(express.json());
