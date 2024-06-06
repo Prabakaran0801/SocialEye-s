9996e96072ea9ff9bad769f16a29f3224dc60436
Prabakaran <karanpk0108@gmail.com>
2024-06-06 12:51:41 +0530

    fix server for vercel deployment

--- server/index.js
+++ server/index.js
@@ -1,3 +1,135 @@
+// import express from "express";
+// import bodyParser from "body-parser";
+// import mongoose from "mongoose";
+// import cors from "cors";
+// import dotenv from "dotenv";
+// import multer from "multer";
+// import helmet from "helmet";
+// import morgan from "morgan";
+// import path from "path";
+// import { fileURLToPath } from "url";
+// import authRoutes from "./routes/auth.js";
+// import userRoutes from "./routes/users.js";
+// import postRoutes from "./routes/posts.js";
+// import { register } from "./controllers/auth.js";
+// import { createPost } from "./controllers/posts.js";
+// import { verifyToken } from "./middleware/auth.js";
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+// dotenv.config();
+// const app = express();
+
+// // Use helmet middleware to set Content Security Policy header
+// // app.use(
+// //   helmet({
+// //     contentSecurityPolicy: {
+// //       directives: {
+// //         ...helmet.contentSecurityPolicy.getDefaultDirectives(),
+// //         "connect-src": ["'self'", "http://localhost:3000"],
+// //         "style-src": ["'self'", "https://fonts.googleapis.com"],
+// //       },
+// //     },
+// //   })
+// // );
+
+// // Remove the custom middleware function for setting CSP header
+// // app.use((req, res, next) => {
+// //   res.setHeader(
+// //     "Content-Security-Policy",
+// //     "default-src 'self'; connect-src http://localhost:3000;"
+// //   );
+// //   next();
+// // });
+
+// app.use((req, res, next) => {
+//   res.setHeader(
+//     "Content-Security-Policy",
+//     "default-src 'self'; style-src 'self' https://fonts.googleapis.com"
+//   );
+//   next();
+// });
+// const corsOptions = {
+//   origin: [
+//     "http://localhost:3006",
+//     "https://social-eyes-e582-e0z26htwe-prabakaran0801s-projects.vercel.app",
+//   ],
+//   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+//   credentials: true,
+//   optionsSuccessStatus: 204,
+// };
+
+// app.use(cors(corsOptions));
+// // Set CSP header middleware
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3006");
+//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+//   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//   next();
+// });
+// app.use(express.json());
+// app.use(bodyParser.json({ limit: "30mb", extended: true }));
+// app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+// app.use(
+//   cors({
+//     origin: "http://localhost:3006",
+//     credentials: true, // If you're using cookies or sessions
+//   })
+// );
+// app.use(helmet());
+// app.use(morgan("common"));
+// app.use(
+//   cors({
+//     origin: "*",
+//     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+//     credentials: true,
+//     optionsSuccessStatus: 204,
+//   })
+// );
+
+// // Serve static files from the React app
+// app.use(express.static(path.join(__dirname, "..", "client", "build")));
+
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "..", "client", "build", "index.html"));
+// });
+
+// // Other routes
+// app.use("/assets", express.static(path.join(__dirname, "/public/assets")));
+
+// // File storage
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, "/public/assets");
+//   },
+//   filename: function (req, file, cb) {
+//     cb(null, file.originalname);
+//   },
+// });
+// const upload = multer({ storage });
+
+// // Routes with files
+// app.post("/auth/register", upload.single("picture"), register);
+// app.post("/posts", verifyToken, upload.single("picture"), createPost);
+
+// // API routes
+// app.use("/auth", authRoutes);
+// app.use("/users", userRoutes);
+// app.use("/posts", postRoutes);
+
+// // Mongoose setup
+// const PORT = process.env.PORT || 3000;
+// mongoose.set("strictQuery", false);
+// mongoose
+//   .connect(process.env.MONGO_URL, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => {
+//     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+//   })
+//   .catch((error) => console.log(`${error} did not connect`));
+
 import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
@@ -20,28 +152,21 @@ const __dirname = path.dirname(__filename);
 dotenv.config();
 const app = express();
 
-// Use helmet middleware to set Content Security Policy header
-// app.use(
-//   helmet({
-//     contentSecurityPolicy: {
-//       directives: {
-//         ...helmet.contentSecurityPolicy.getDefaultDirectives(),
-//         "connect-src": ["'self'", "http://localhost:3000"],
-//         "style-src": ["'self'", "https://fonts.googleapis.com"],
-//       },
-//     },
-//   })
-// );
+// Configure CORS options
+const corsOptions = {
+  origin: [
+    "http://localhost:3006",
+    "https://social-eyes-e582-e0z26htwe-prabakaran0801s-projects.vercel.app",
+  ],
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
 
-// Remove the custom middleware function for setting CSP header
-// app.use((req, res, next) => {
-//   res.setHeader(
-//     "Content-Security-Policy",
-//     "default-src 'self'; connect-src http://localhost:3000;"
-//   );
-//   next();
-// });
+// Apply CORS middleware
+app.use(cors(corsOptions));
 
+// Set CSP header middleware
 app.use((req, res, next) => {
   res.setHeader(
     "Content-Security-Policy",
@@ -49,22 +174,10 @@ app.use((req, res, next) => {
   );
   next();
 });
-// Set CSP header middleware
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3006");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
+
 app.use(express.json());
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(
-  cors({
-    origin: "http://localhost:3006",
-    credentials: true, // If you're using cookies or sessions
-  })
-);
 app.use(helmet());
 app.use(morgan("common"));
 
@@ -81,7 +194,7 @@ app.use("/assets", express.static(path.join(__dirname, "/public/assets")));
 // File storage
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "/public/assets");
+    cb(null, "public/assets");
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
